9e332a0d1ba5f569282ef99292330620e1e625f6
Zack Bumpous <upwork.ztb@gmail.com>
2019-11-01 10:48:34 -0500

    Might have to tweak app as I build out components and what not but pretty sure I got everything layed out how I want it to look, did my axios call and set state in app js, gonna start building a playercard and navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//component imports will go below when finished making them, gonna make a PlayerCard and a Navbar. Will call for them now I am gonna build out app js
+
+class App extends React.Component{
+  //need state
+  state = {
+    player: [],
+  }
+
+  componentDidMount(){
+    //need our axios
+    axios
+    .get(`http://localhost:5000/api/players`)
+    .then(res =>{
+      console.log(` the axios call returned:`, res.data)
+      console.log(`res.data type is:`, typeof res.data)
+
+      this.setState({
+        player: res.data
+      })
+    })
+  }
+  render(){
+    console.log(this.state.player)
+    return (
+      <div>
+        <Navbar />
+        <header>
+          <h1>Testing, attention please!</h1>
+          <div>
+            {this.state.player.map((player, key) => {
+              console.log(player);
+              return <PlayerCard key = {key} player = {player} />
+            })}
+          </div>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
